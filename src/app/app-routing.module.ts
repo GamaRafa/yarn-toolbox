27224d8f034d9b61316e94980843fe7aec2d6bf3
GamaRafa <rafa.gama.em7@gmail.com>
2024-01-04 19:51:27 -0300

    Navegação finalmente funcionando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,43 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './features/home/components/home.component';
+import { YarnStashComponent } from './features/yarn-stash/components/yarn-stash.component';
+import { RepCounterComponent } from './features/rep-counter/components/rep-counter.component';
+import { RowCounterComponent } from './features/row-counter/components/row-counter.component';
+import { IncreaseCalculatorComponent } from './features/increase-calculator/components/increase-calculator.component';
+import { DecreaseCalculatorComponent } from './features/decrease-calculator/components/decrease-calculator.component';
 
 const routes: Routes = [
   {
     path: '',
-    redirectTo: 'folder/home',
+    redirectTo: 'home',
     pathMatch: 'full'
   },
   {
-    path: 'folder/:id',
-    loadChildren: () => import('./folder/folder.module').then( m => m.FolderPageModule)
+    path: 'home',
+    component: HomeComponent
   },
+  {
+    path: 'yarn-stash',
+    component: YarnStashComponent
+  },
+  {
+    path: 'row-counter',
+    component: RowCounterComponent
+  },
+  {
+    path: 'rep-counter',
+    component: RepCounterComponent
+  },
+  {
+    path: 'increase-calculator',
+    component: IncreaseCalculatorComponent
+  },
+  {
+    path: 'decrease-calculator',
+    component: DecreaseCalculatorComponent
+  }
+
 
 ];
 
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  // public appPages = [
-  //   { title: 'Inbox', url: '/folder/inbox', icon: 'mail' },
-  //   { title: 'Outbox', url: '/folder/outbox', icon: 'paper-plane' },
-  //   { title: 'Favorites', url: '/folder/favorites', icon: 'heart' },
-  //   { title: 'Archived', url: '/folder/archived', icon: 'archive' },
-  //   { title: 'Trash', url: '/folder/trash', icon: 'trash' },
-  //   { title: 'Spam', url: '/folder/spam', icon: 'warning' },
-  // ];
 
-  public appPages = [
-    { title: 'Home', url: '/folder/home'},
-    { title: 'Yarn Stash', url: '/folder/yarn-stash'},
-    { title: 'Contador de carreiras', url: '/folder/row-counter'},
-    { title: 'Contador de repetições', url: '/folder/rep-counter'},
-    { title: 'Calculadora de aumentos', url: '/folder/increase-calculator'},
-    { title: 'Calculadora de diminuições', url: '/folder/decrease-calculator'},
+  title = 'Home'
+  
+  appPages = [
+    {
+      title: 'Home',
+      path: 'home'
+    },
+    {
+      title: 'Yarn Stash',
+      path: 'yarn-stash'
+    },
+    {
+      title: 'Contador de carreiras',
+      path: 'row-counter'
+    },
+    {
+      title: 'Contador de repetições',
+      path: 'rep-counter'
+    },
+    {
+      title: 'Calculadora de aumentos',
+      path: 'increase-calculator'
+    },
+    {
+      title: 'Calculadora de diminuições',
+      path: 'decrease-calculator'
+    },
 
   ]
-  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
   constructor() {}
+
 }
